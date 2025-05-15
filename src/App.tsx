3ba8caa8ab9e2cb5ead69f7d2db44525474fa8ef
gh-johnny <jmfurtadoromero@gmail.com>
2025-05-15 08:58:49 -0300

    comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+// ----------------------------------------------------------
+//    TODO:
+//
+//    [ ] - Fazer requisição para a rota "albums/1/photos" 
+//    [ ] - Tratar erro com um simples console.error
+//    [ ] - Fazer um estado de loading (mostrar um simples "Carregando..." na tela.)
+//    [ ] - Listar os dados retornados em tela
+//    [ ] - Crie um componente que será utilizado na listagem dos dados
+//    [ ] - Estilize minimamente o conteúdo
+//
+//    OBS:
+//    - Sinta-se à vontade para estilizar e alterar a estrutura atual
+// ----------------------------------------------------------
+
 function App() {
   return (
     <div className="bg-sky-50 w-dvw h-dvh">
@@ -7,13 +21,17 @@ function App() {
 
       <div>
         {[].map(() => (
-          <div>
+          <>
+            {/* Substitua aqui por um componente customizado (começo) */}
+            <div>
 
-            <h3>
-            </h3>
-            <img />
+              <h3>
+              </h3>
+              <img />
 
-          </div>
+            </div>
+            {/* Substitua aqui por um componente customizado (fim) */}
+          </>
         ))}
       </div>
     </div>
